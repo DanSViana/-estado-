a02b6e0f0eb75e95f1740515d8a4c6f6a0abd5cc
DanSViana <sousadaniel208@gmail.com>
2024-08-02 17:47:50 -0300

    Versão 1.0

---
+++ estado_app.py
@@ -0,0 +1,7 @@
+'''
+Crie um programa que imprima uma lista com todos os estados do Brasil. Ao terminar, crie um repositório, envie para o GitHub e poste o link do repositório remoto.
+'''
+estados = ['Distrito Federal - DF','Goiás - GO','Mato Grosso - MT','Mato Grosso do Sul - MS','Alagoas - AL','Bahia - BA','Ceará - CE','Maranhão - MA','Paraíba - PB','Pernambuco - PE','Piauí - PI','Rio Grande do Norte - RN','Sergipe - SE','Acre - AC','Amapá - AP','Amazonas - AM','Pará - PA','Rondônia - RO','Roraima - RR','Tocantis - TO','Espírito Santo - ES','Minas Gerais - MG','Rio de Janeiro -RJ','São Paulo - SP','Paraná - PR','Rio Grande do Sul - RS','Santa Catarina - SC']
+
+for i in range(27):
+    print(f'{i + 1}º estado: {estados[i]}.')
